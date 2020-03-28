a28e08322910df0da98cc8001745e7ddea0f0618
D R Raghuram <raghuramdr@users.noreply.github.com>
2020-03-28 11:07:18 +0530

    Update kaam24.py
    Renamed variables to make code more readable.

--- kaam24/Kaam24.py
+++ kaam24/Kaam24.py
@@ -5,11 +5,13 @@ Created on Fri Mar  6 22:42:51 2020
 @author: shass
 """
 
+url_ = "https://www.kaam24.com/Jobs/jobs-in-India"
+
 import pandas as pd
 from bs4 import BeautifulSoup
 from selenium import webdriver
 import time
-#%%%%
+
 def scroll_down(D):
     """A method for scrolling the page."""
 
@@ -32,20 +34,19 @@ def scroll_down(D):
             break
 
         last_height = new_height
-#%%%%%
 
 driver = webdriver.Chrome()
-driver.get("https://www.kaam24.com/Jobs/jobs-in-India")
+driver.get(url_)
 time.sleep(5)
 scroll_down(driver)
 
-#%%%%%%%%%%
+
 Jobheading=[]
 Position=[]
 Data=[]
 content = driver.page_source
 soup = BeautifulSoup(content)
-#%%%%%
+
 for a in soup.findAll('div',href=False, attrs={'class':'center-outer card'}):
     JT=a.find('div', attrs={'class':'nameheading'})
     P=a.find('div', attrs={'class':'namecat'})
@@ -53,6 +54,7 @@ for a in soup.findAll('div',href=False, attrs={'class':'center-outer card'}):
     Jobheading.append(JT.text)
     Position.append(P.text)
     Data.append(L.text.split("\n"))
+    
 for a in soup.findAll('div',href=False, attrs={'class':'center-outer card ng-scope'}):
     JT=a.find('div', attrs={'class':'nameheading'})
     P=a.find('div', attrs={'class':'namecat'})
@@ -62,7 +64,7 @@ for a in soup.findAll('div',href=False, attrs={'class':'center-outer card ng-sco
     Data.append(L.text.split("\n"))
 
 #%%%%%
-AllData=pd.DataFrame({'Job':Jobheading,'Position':Position,'Data':Data})
+df=pd.DataFrame({'Job':Jobheading,'Position':Position,'Data':Data})
 
 # Blow line has been updated to have a unique CSV delimiter `|||`
-AllData.to_csv('kaam_24.csv', sep='|||', index=False)
+df.to_csv('kaam_24.csv', sep='|||', index=False)
